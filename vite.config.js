1bdea53cf843e6bb112e4e1c875df517d857d7e2
Jacob Calvillo <jakeob99@gmail.com>
2024-08-03 12:18:19 -0600

    adding earth

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite';
-
+import { resolve } from 'path'
 
 export default defineConfig({
     base: 'https://jacobcalvillo.github.io/ProyectoMult/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                earth: resolve(__dirname, 'earth.html'),
+            },
+        },
+    },	
 });
